007bca7d07b1b6697e21c289ac031ce85841e561
thomasthomsen16 <35918236+thomasthomsen16@users.noreply.github.com>
2025-05-08 14:49:40 +0200

    Sanitize inputs to constructor

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 const myLibrary = [];
 
 function Book(author,title,pages,read){
+    if (!new.target) {
+        throw Error("You must use the 'new' operator to call the constructor");
+      }
     this.ID = crypto.randomUUID();
-    this.author = author;
-    this.title = title;
-    this.pages = pages;
-    this.read = read;
+    this.author = String(author).trim();
+    this.title = String(title);
+    this.pages = Number(pages);
+    this.read = Boolean(read);
 };
 
 function addBookToLibrary(author,title,pages,read){
