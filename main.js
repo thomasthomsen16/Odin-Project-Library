e37b819a1e45e6fbf718f9c91b40c5b54310f21d
thomasthomsen16 <35918236+thomasthomsen16@users.noreply.github.com>
2025-05-11 12:08:52 +0200

    Create modal that adds books and displays them in table

--- main.js
+++ main.js
@@ -33,8 +33,36 @@ function displayBooks() {
         });
         bookTable.appendChild(row);
     });
-
 }
+
+// Open modal
+document.getElementById("addBook").addEventListener("click", () => {
+    const dialog = document.getElementById("dialog");
+    dialog.showModal();
+});
+
+//Close modal
+document.getElementById("cancelDialog").addEventListener("click", () => {
+    const dialog = document.getElementById("dialog");
+    dialog.close();
+});
+
+
+document.getElementById("bookForm").addEventListener("submit", (e) => {
+    e.preventDefault(); // Prevent form from submitting to a server
+
+    const form = e.target;
+    const author = form.author.value;
+    const title = form.title.value;
+    const pages = form.pages.value;
+    const read = form.read.checked;
+
+    addBookToLibrary(author, title, pages, read);
+    displayBooks();
+
+    form.reset(); // Clear form inputs
+    dialog.close();
+});
 
 book1=addBookToLibrary("ksdfh", "sldfjksdf",39,false)
 book2=addBookToLibrary("haha", "yaya",45,true)
